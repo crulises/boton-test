c5e9707eb84c493c4ac17b1c47c632328aec3726
Uli <ulisesrcolina@gmail.com>
2025-02-21 19:40:56 -0300

    Agrego comentarios a las funciones

--- Python/matematica.py
+++ Python/matematica.py
@@ -1,13 +1,17 @@
 def suma(a, b):
+    """Suma dos valores"""
     return a + b
 
 def resta(a, b):
+    """Resta dos valores"""
     return a - b
 
 def multiplicacion(a, b):
+    """Multiplica dos valores"""
     return a * b
 
 def division(a, b):
+    """Divide dos valores"""
     if b == 0:
         return "Error: División por cero no permitida"
     return a / b
